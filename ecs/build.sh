277950cb1db5ab75f62a7ce5b419f6d51eaae686
Alex Raju <alex@qburst.com>
2017-03-22 05:27:57 +0530

    Added CloudWatch Alarms & Log groups

--- ecs/build.sh
+++ ecs/build.sh
@@ -1,17 +1,15 @@
 #!/usr/bin/env bash
 if [ "$DEPLOY_ENVIRONMENT" != "production" ]; then
     echo -n "$CODEBUILD_BUILD_ID" | sed "s/.*:\([[:xdigit:]]\{7\}\).*/\1/" > build.id
-    echo -n "RELEASE_VERSION-$BUILD_SCOPE-$(cat ./build.id)" > docker.tag
+    echo -n "$RELEASE_VERSION-$BUILD_SCOPE-$(cat ./build.id)" > docker.tag
     docker build -t $AWS_ACCOUNT_ID.dkr.ecr.$AWS_REGION.amazonaws.com/$ECR_NAME:$(cat docker.tag) .
     TAG=$(cat docker.tag)
 else
     TAG=$RELEASE_VERSION
 fi
 
-
 sed -i "s@TAG@$TAG@g" ecs/service.yaml
-
+sed -i "s#EMAIL#$EMAIL#g" ecs/service.yaml
 sed -i "s@ENVIRONMENT_NAME@$ENVIRONMENT_NAME@g" ecs/service.yaml
 sed -i "s@DOCKER_IMAGE_URI@$AWS_ACCOUNT_ID.dkr.ecr.$AWS_REGION.amazonaws.com/$ECR_NAME:$TAG@g" ecs/service.yaml
-
 sed -i "s@BUILD_SCOPE@$BUILD_SCOPE@g" ecs/service.yaml
