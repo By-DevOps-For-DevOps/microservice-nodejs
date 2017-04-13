4fb1301d89c232b2ee9869c318ebac09fa36a563
Amal-V <amalv@qburst.com>
2017-04-14 00:18:53 +0530

    updating build

--- ecs/post_build.sh
+++ ecs/post_build.sh
@@ -3,6 +3,7 @@ if [ "$DEPLOY_ENVIRONMENT" = "development" ] || \
    [ "$DEPLOY_ENVIRONMENT" = "staging" ] || \
    [ "$DEPLOY_ENVIRONMENT" = "feature" ] || \
    [ "$DEPLOY_ENVIRONMENT" = "hotfix" ]; then
+    $(aws ecr get-login --registry-ids "${AWS_ACCOUNT_ID}" --region $AWS_REGION)
     echo ${AWS_ACCOUNT_ID}.dkr.ecr.${AWS_REGION}.amazonaws.com/${ECR_NAME}:$(cat ./docker.tag)
     docker push ${AWS_ACCOUNT_ID}.dkr.ecr.${AWS_REGION}.amazonaws.com/${ECR_NAME}:$(cat ./docker.tag)
 elif [ "$DEPLOY_ENVIRONMENT" = "production" ]; then
